5bbfad77192a5d6526a798417cd40ebdf78c2c31
okb97 <hercule0903@icloud.com>
2025-09-16 21:05:10 +0900

    confirm import sentence

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import {useEffect, useState} from 'react';
 
 export default function Home() {
