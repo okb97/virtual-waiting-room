2482176e09a8215392db819f11a290f87d643d42
okb97 <hercule0903@icloud.com>
2025-09-17 00:35:00 +0900

    confirm vercel.json

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/purchase',
-        destination: '/purchase',
-        permanent: false,
-      },
-    ]
-  },
-};
-
+}
 export default nextConfig;
