0b39f518ce2d954ef8d3874d4cc588db6fea57f5
okb97 <hercule0903@icloud.com>
2025-09-16 21:48:09 +0900

    confirm next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/purchase',
+        destination: '/purchase',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
